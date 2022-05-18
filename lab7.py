63d246204c8599d208709f214bfdb7e4659686ac
Híthalo César <hithalocesar@gmail.com>
2022-05-18 01:25:51 -0300

    Optimize functions

--- lab7.py
+++ lab7.py
@@ -1,66 +1,46 @@
-from typing import List
+from typing import List, Tuple
+# O professor Rafael Schouery autorizou a importação dessa biblioteca.
 
 
-def without_equal_consecutive(list_: List[str]):
-    """Dada uma lista de strings, retorna uma nova lista de strings com os
-    mesmos elementos, mas sem elementos iguais consecutivos.
-    """
-    new_list = [list_[0]]
-    for element in list_:
-        if element != new_list[-1]:
-            new_list.append(element)
-    return new_list
-
-
-def most_consecutive_occurrence(list_: List[str]):
-    """Dada uma lista de strings, retorna o elemento que se mais se repete
+def longest_repetition(list_: List[str]) -> Tuple[str, int]:
+    """Dada uma lista de strings, retorna a string que se mais se repete
     de forma consecutiva e quantas vezes ela se repete.
     """
-    no_equal_consecutive = without_equal_consecutive(list_)
-    element_count = []
-    starting_index = 0
-    for i in range(len(no_equal_consecutive)):
-        element_count.append(0)
-        for j in range(starting_index, len(list_)):
-            if list_[j] == no_equal_consecutive[i]:
-                element_count[i] += 1
-                starting_index += 1
-            else:
-                break
+    current_repetition = max_repetition = 1
+    for i in range(len(list_) - 1):
+        if list_[i] == list_[i + 1]:
+            current_repetition += 1
+        else:
+            if current_repetition > max_repetition:
+                max_repetition = current_repetition
+                max_repetition_string = list_[i]
+            current_repetition = 1
     return (
-        no_equal_consecutive[element_count.index(max(element_count))],
-        (max(element_count))
+        max_repetition_string, max_repetition
     )
 
 
-def remove_equal(list_: List[str]):
-    """Dada uma lista de strings, retorna uma nova lista de strings com os
-    mesmos elementos, mas sem repetição.
+def set_(list_: List[str]) -> List[str]:
+    """Dada uma lista de strings, retorna uma nova lista com as mesmas strings,
+    mas sem repetição.
     """
     new_list = []
-    for element in list_:
-        if element not in new_list:
-            new_list.append(element)
+    for string in list_:
+        if string not in new_list:
+            new_list.append(string)
     return new_list
 
 
-def count_unique_elements(list_: List[str]):
-    """Dada uma lista de strings, retorna o número de elementos únicos na
-    lista.
-    """
-    return len(remove_equal(list_))
-
-
-def kebab_case_1_start(list_: List[str], start_after: str):
-    """Dada uma lista de strings, converete seus elementos para kebab-case a
+def kebab_case(list_: List[str], start_after: str) -> None:
+    """Dada uma lista de strings, converete seus objetos para kebab-case a
     partir de um caractere específico.
     """
-    for element_index in range(len(list_)):
-        renamed_element = ""
+    for string_index in range(len(list_)):
+        renamed_string = ""
         start_conversion = False
-        for char in list_[element_index]:
+        for char in list_[string_index]:
             if not start_conversion:
-                renamed_element += char
+                renamed_string += char
                 if char == start_after:
                     start_conversion = True
             else:
@@ -68,21 +48,22 @@ def kebab_case_1_start(list_: List[str], start_after: str):
                     char = char.lower()
                 elif char == " ":
                     char = "-"
-                renamed_element += char
-        list_[element_index] = renamed_element
+                renamed_string += char
+        list_[string_index] = renamed_string
 
 
 def main():
+    """Executa o programa principal."""
     photos_str, photo_to_remove = input().split("/ ")
     photos = photos_str.split(", ")
-    most_consecutive_photo, count = most_consecutive_occurrence(photos)
+    most_consecutive_photo, count = longest_repetition(photos)
     print(most_consecutive_photo, count)
-    print(count_unique_elements(photos))
+    photos = set_(photos)
+    print(len(photos))
     photos = [
         photo for photo in photos if photo != photo_to_remove
     ]
-    photos = remove_equal(photos)
-    kebab_case_1_start(photos, "_")
+    kebab_case(photos, "_")
     print([photo for photo in photos if photo[:2] == "HA"])
     print([photo for photo in photos if photo[:2] == "CR"])
     print([photo for photo in photos if photo[:2] == "CC"])
